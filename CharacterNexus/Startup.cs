027138cd9bf88ddac5254c6e3808ea29d4a367cf
tools81 <tooleyman88@yahoo.com>
2026-01-16 22:45:01 -0600

    Forgot to remove spa using

--- CharacterNexus/Startup.cs
+++ CharacterNexus/Startup.cs
@@ -1,15 +1,10 @@
 using Utility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
-using System;
 
 namespace CharacterNexus
 {
